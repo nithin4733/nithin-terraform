f91954a2855eaed16aec3b757a24b9e75aaee9ff
Nithin <nithin.nerella@gmail.com>
2023-07-09 15:47:19 +0530

    Updated Var file in RG

--- RG/variables.tf
+++ RG/variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group_location" {
-  default     = "eastus"
+  default     = "__RGLocation__"
   description = "Location of the resource group."
 }
 
 variable "resource_group_name" {
-  default     = "NithintestRG"
+  default     = "__RGName__"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
